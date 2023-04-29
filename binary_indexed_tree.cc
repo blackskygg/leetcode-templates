a921bbe87607b9120c9e05df8f209197517ca5e7
Zhongze Liu <blackskygg@google.com>
2023-04-30 00:24:06 +0800

    Add BIT and UnionFind

---
+++ binary_indexed_tree.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BinaryIndexedTree {
+ public:
+  // Initializes the tree with `n` elements.
+  BinaryIndexedTree(int64_t n) : sums_(n+1, 0) {}
+
+  // Adds `n` to the element on `pos` (1-based).
+  void Update(int64_t pos, int64_t n) {
+    while (pos < sums_.size()) {
+      sums_[pos] += n;
+      pos += LowBit(pos);
+    }
+  }
+
+  // Returns the sum of elements from position 1 to `pos` (1-based).
+  int64_t Query(int64_t pos) {
+    int64_t ans = 0;
+    while (pos > 0) {
+      ans += sums_[pos];
+      pos -= LowBit(pos);
+    }
+    return ans;
+  }
+
+ private:
+  inline int64_t LowBit(int64_t x) {
+    return x & -x;
+  }
+
+ private:
+  vector<int64_t> sums_;
+};
---
+++ union_find.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class UnionFind {
+ public:
+  // Constructs an instance with `n` elements.
+  UnionFind(int n) {
+    parent_ = vector<int>(n);
+    for (int i = 0; i < n; ++i) {
+      parent_[i] = i;
+    }
+
+    rank_ = vector<int>(n, 1);
+    size_ = vector<int64_t>(n, 1);
+  }
+
+  // Returns the parent of `x`'s cluster.
+  int Find(int x) {
+    if (x == parent_[x]) {
+      return x;
+    } else {
+      return parent_[x] = Find(parent_[x]);
+    }
+  }
+
+  // Unions `x`'s and `y`'s cluster and returns the new parent.
+  int Union(int x, int y) {
+    int px = Find(x), py = Find(y);
+    if (px == py) return px;
+
+    if (rank_[px] > rank_[py]) swap(px, py);
+    if (rank_[px] == rank_[py]) rank_[py]++;
+    size_[py] += size_[px];
+    parent_[px] = py;
+    return py;
+  }
+
+  // Returns the size of `x`'s cluster.
+  int64_t Size(int x) {
+    return size_[x];
+  }
+
+ private:
+  vector<int> parent_, rank_;
+  vector<int64_t> size_;
+};
