08feef874f04b531c5d4f5b10d5c1ddc242f1b1e
Zhongze Liu <blackskygg@google.com>
2023-05-02 23:14:50 +0800

    Add StrSplit

---
+++ string_utils.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Splits `s` by `delim`. Will not return empty string if `skip_empty` is true.
+template <typename T>
+vector<string_view> StrSplit(const string_view s, T&& delim,
+                             const bool skip_empty = false) {
+  vector<string_view> parts;
+  int start = 0;
+  while (start < s.size()) {
+    auto pos = s.find(forward<T>(delim), start);
+    if (pos == string_view::npos) pos = s.size();
+    if (!skip_empty || pos != start) {
+      parts.push_back(s.substr(start, pos-start));
+    }
+    start = pos + 1;
+  }
+  return parts;
+}
